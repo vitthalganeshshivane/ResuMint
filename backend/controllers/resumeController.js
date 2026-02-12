83237665d3ade1f20b5a4536dd05645a35b61eef
vitthal4545 <vitthalgs1434@gmail.com>
2026-02-12 23:19:10 +0530

    feat(backend): add protected create resume endpoint with default template structure

---
+++ backend/controllers/resumeController.js
@@ -0,0 +1,94 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const Resume = require("../models/Resume");
+
+const createResume = async (req, res) => {
+  try {
+    const { title } = req.body;
+
+    const defaultResumeData = {
+      profileInfo: {
+        profileImg: null,
+        previewUrl: "",
+        fullName: "",
+        designation: "",
+        summary: "",
+      },
+      contactInfo: {
+        email: "",
+        phone: "",
+        location: "",
+        linkedin: "",
+        github: "",
+        website: "",
+      },
+      workExperience: [
+        {
+          company: "",
+          roles: "",
+          startDate: "",
+          endDate: "",
+          description: "",
+        },
+      ],
+      education: [
+        {
+          degree: "",
+          institution: "",
+          startDate: "",
+          endDate: "",
+        },
+      ],
+
+      skills: [
+        {
+          name: "",
+          progress: 0,
+        },
+      ],
+      projects: [
+        {
+          title: "",
+          description: "",
+          github: "",
+          liveDemo: "",
+        },
+      ],
+      certifications: [
+        {
+          title: "",
+          issuer: "",
+          year: "",
+        },
+      ],
+
+      languages: [
+        {
+          name: "",
+          progress: 0,
+        },
+      ],
+      interests: [""],
+    };
+
+    const newResume = await Resume.create({
+      userId: req.user._id,
+      title,
+      ...defaultResumeData,
+    });
+
+    res.status(201).json(newResume);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to create resume", error: error.message });
+  }
+};
+
+module.exports = {
+  createResume,
+  getUserResumes,
+  getResumeById,
+  updateResume,
+  deleteResume,
+};
---
+++ backend/routes/resumeRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { protect } = require("../middlewares/authMiddleware");
+const { uploadResumeImages } = require("../controllers/uploadImages");
+const {
+  createResume,
+  getUserResumes,
+  getResumeById,
+  updateResume,
+  deleteResume,
+} = require("../controllers/resumeController");
+const router = express.Router();
+
+router.post("/", protect, createResume);
+
+module.exports = router;
