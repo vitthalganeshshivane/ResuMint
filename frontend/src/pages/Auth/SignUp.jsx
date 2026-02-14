bcf3b62136cd704fe74ca216c113f0b39995409f
vitthal4545 <vitthalgs1434@gmail.com>
2026-02-14 21:29:33 +0530

    feat(frontend): implement signup page with profile image upload and registration flow

---
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -0,0 +1,129 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
+import Input from "../../components/inputs/Input";
+import ProfilePhotoSelector from "../../components/inputs/ProfilePhotoSelector";
+import { UserContext } from "../../context/userContext";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import uploadImage from "../../utils/uploadImage";
+
+const SignUp = ({ setCurrentPage }) => {
+  const [profilePic, setProfilePic] = useState(null);
+
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState();
+
+  const { updateUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Please enter full name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    try {
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name: fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+
+      const { token } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+        navigate("/dashboard");
+      }
+      console.log("signup");
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again");
+      }
+    }
+  };
+
+  return (
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Create An Account</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Join us today by entering your details below.
+      </p>
+
+      <form onSubmit={handleSignUp} action="">
+        <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-2">
+          <Input
+            value={fullName}
+            onChange={({ target }) => setFullName(target.value)}
+            label="Full Name"
+            placeholder="John"
+            type={"text"}
+          />
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type={"text"}
+          />
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type={"password"}
+          />
+        </div>
+
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+        <button type="submit" className="btn-primary">
+          SIGN UP
+        </button>
+
+        <p className="text-[13px] text-slate-800 mt-3">
+          Already an account ?{" "}
+          <button
+            className="font-medium text-primary underline cursor-pointer"
+            onClick={() => setCurrentPage("login")}
+          >
+            Login{" "}
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
