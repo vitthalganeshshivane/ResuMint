0a28690a8e913b49ddb5a3c5fd9867aff86a21dc
vitthal4545 <vitthalgs1434@gmail.com>
2026-02-17 14:17:16 +0530

    refactor(backend): move multer fields configuration to resume upload route

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { protect } = require("../middlewares/authMiddleware");
+const upload = require("../middlewares/uploadMiddleware");
 const { uploadResumeImages } = require("../controllers/uploadImages");
 const {
   createResume,
@@ -14,7 +15,15 @@ router.post("/", protect, createResume);
 router.get("/", protect, getUserResumes);
 router.get("/:id", protect, getResumeById);
 router.put("/:id", protect, updateResume);
-router.put("/:id/upload-images", protect, uploadResumeImages);
+router.put(
+  "/:id/upload-images",
+  protect,
+  upload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "thumbnail", maxCount: 1 },
+  ]),
+  uploadResumeImages,
+);
 
 router.delete("/:id", protect, deleteResume);
 
