93342f274ceeb69de6d1c1c45206808c998ff64e
vitthal4545 <vitthalgs1434@gmail.com>
2026-02-19 13:08:56 +0530

    feat(frontend): add editable resume title component

---
+++ frontend/src/components/inputs/TitleInput.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { LuCheck, LuPencil } from "react-icons/lu";
+
+const TitleInput = ({ title, setTitle, onSave }) => {
+  const [showInput, setShowInput] = useState(false);
+
+  const handleSave = async () => {
+    if (onSave) {
+      await onSave();
+    }
+    setShowInput(false);
+  };
+
+  return (
+    <div className="flex items-center gap-3">
+      {showInput ? (
+        <>
+          <input
+            type="text"
+            placeholder="Resume title"
+            className="text-sm md:text-[17px] bg-transparent outline-none text-black font-semibold border-b border-gray-300 pb-1"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+
+          <button className="cursor-pointer">
+            <LuCheck
+              className="text-[16px] text-purple-600"
+              onClick={handleSave}
+            />
+          </button>
+        </>
+      ) : (
+        <>
+          <h2 className="text-sm md:text-[17px] font-semibold">{title}</h2>
+          <button className="cursor-pointer">
+            <LuPencil
+              className="text-sm text-purple-600"
+              onClick={() => setShowInput((prevState) => !prevState)}
+            />
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TitleInput;
