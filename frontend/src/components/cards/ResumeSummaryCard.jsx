f6728e77c0a2f58d3296ef914e802f52efcefb94
vitthal4545 <vitthalgs1434@gmail.com>
2026-02-21 17:02:54 +0530

    feat(frontend): add resume summary card with dynamic background styling

---
+++ frontend/src/components/cards/ResumeSummaryCard.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { getLightColorFromImage } from "../../utils/helper";
+
+const ResumeSummaryCard = ({ imgUrl, title, lastUpdated, onSelect }) => {
+  const [bgColor, setBgColor] = useState("#ffffff");
+
+  useEffect(() => {
+    if (imgUrl) {
+      getLightColorFromImage(imgUrl)
+        .then((color) => {
+          setBgColor(color);
+        })
+        .catch(() => {
+          setBgColor("#ffffff");
+        });
+    }
+  }, [imgUrl]);
+
+  return (
+    <div
+      onClick={onSelect}
+      className="h-[300px] flex flex-col items-center justify-between bg-white rounded-lg border border-gray-200  hover:border-purple-300 overflow-hidden cursor-pointer"
+      style={{ backgroundColor: bgColor }}
+    >
+      <div className="p-4">
+        {imgUrl ? (
+          <img src={imgUrl} className="w-[100%] h-[200px] rounded" alt="" />
+        ) : (
+          <div className=""></div>
+        )}
+      </div>
+
+      <div className="w-full bg-white px-4 py-3">
+        <h5 className="text-sm font-medium truncate overflow-hidden whitespace-nowrap">
+          {title}
+        </h5>
+        <p className="text-xs font-medium text-gray-500 mt-0.5">
+          Last Updated: {lastUpdated}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ResumeSummaryCard;
