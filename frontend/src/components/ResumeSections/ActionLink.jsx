d6053f53cae80fc72f89129747a7d7d418f95485
vitthal4545 <vitthalgs1434@gmail.com>
2026-02-23 18:54:27 +0530

    fix(frontend): make action links open in new tab

--- frontend/src/components/ResumeSections/ActionLink.jsx
+++ frontend/src/components/ResumeSections/ActionLink.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const ActionLink = ({ icon, link, bgColor }) => {
   return (
-    <div className="flex items-center gap-3">
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center gap-3"
+    >
       <div
         className="w-[25px] h-[25px] flex items-center justify-center rounded-full"
         style={{ backgroundColor: bgColor }}
@@ -13,7 +18,7 @@ const ActionLink = ({ icon, link, bgColor }) => {
       <p className="text-[13px] font-medium underline cursor-pointer break-all">
         {link}
       </p>
-    </div>
+    </a>
   );
 };
 
