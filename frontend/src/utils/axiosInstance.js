a4a76c213bf797c184efe59c37da2d9bec47ea46
Vitthal Ganesh Shivane <vitthalgs1434@gmail.com>
2026-08-07 23:45:02 +0530

    fix: axios interceptor request.use -> response.use to handle 401 properly

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -24,18 +24,19 @@ axiosInstance.interceptors.request.use(
   },
 );
 
-axiosInstance.interceptors.request.use(
+axiosInstance.interceptors.response.use(
   (response) => {
     return response;
   },
   (error) => {
     if (error.response) {
       if (error.response.status === 401) {
+        localStorage.removeItem("token");
         window.location.href = "/";
       } else if (error.response.status === 500) {
         console.error("Server error. Please try again later");
       } else if (error.code === "ECONNABORTED") {
-        console.error("Request timeout. Please try again");
+        console.error("Request timeout. Please try again later");
       }
     }
 
